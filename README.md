0c05cf84a41da881659d074b1268c925a69e0fe5
dayan-noetic <dayan.mendis@wearenoetic.com>
2020-06-29 13:46:22 +0530

    change the readme file with function.php code lines

--- README.md
+++ README.md
@@ -294,6 +294,23 @@ npm install cross-env --save-dev
 npm run watch
 ```
 
+> Add following code to function.php
+```shell
+function load_css() {
+    wp_register_style('app', get_template_directory_uri() . '/dist/app.css', array(), false, 'all' );
+    wp_enqueue_style('app');
+}
+add_action( 'wp_enqueue_scripts', 'load_css' );
+
+function load_js()
+{
+    wp_enqueue_script('jquery');
+    wp_register_script('main', get_template_directory_uri() . '/dist/app.js', array( 'jquery' ), false, true);
+    wp_enqueue_script('main');
+}
+add_action('wp_enqueue_scripts', 'load_js');
+```
+
 
 
 
--- src/web/wp-config.php
+++ src/web/wp-config.php
@@ -6,13 +6,15 @@
  */
 
 /* Multisite */
-define( 'WP_ALLOW_MULTISITE', true );
-define('MULTISITE', false); /* Make this true to make Multisite */
-define('SUBDOMAIN_INSTALL', false);
-define('DOMAIN_CURRENT_SITE', 'myapp.local');
+//define( 'WP_ALLOW_MULTISITE', true );
+//define('MULTISITE', true);
+//define('SUBDOMAIN_INSTALL', false);
+//define('DOMAIN_CURRENT_SITE', 'myapp.local');
 //define('PATH_CURRENT_SITE', '/wp/');
-define('SITE_ID_CURRENT_SITE', 1);
-define('BLOG_ID_CURRENT_SITE', 1);
+//define('SITE_ID_CURRENT_SITE', 1);
+//define('BLOG_ID_CURRENT_SITE', 1);
+
+
 
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 require_once dirname(__DIR__) . '/config/application.php';
